0b517d97ed85876fedcde7494ab19d398a1c66e1
Rose Cass <rose.cass@shopify.com>
2021-01-14 11:05:20 -0800

    Added polaris page component

--- polaris-practice/src/App.js
+++ polaris-practice/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import enTranslations from "@shopify/polaris/locales/en.json";
+import { AppProvider, Page, Layout, Card } from "@shopify/polaris";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppProvider i18n={enTranslations}>
+        <body>
+          <Page title="Settings" separator>
+            <Layout>
+              <Card title="Online store dashboard" sectioned>
+                <p>View a summary of your online store’s performance.</p>
+              </Card>
+            </Layout>
+          </Page>
+        </body>
+      </AppProvider>
     </div>
   );
 }
